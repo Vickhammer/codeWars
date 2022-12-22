447dc354197bb369beea4cd188f7b5397283f6c4
Vickhammer <tvickhammer@gmail.com>
2022-12-22 12:00:00 -0700

    Kyu 7

---
+++ Kyu 7/Next Palindromic Number.js
@@ -0,0 +1,26 @@
+// DESCRIPTION:
+// There were and still are many problem in CW about palindrome numbers and palindrome strings. We suposse that you know which kind of numbers they are. If not, you may search about them using your favourite search engine.
+
+// In this kata you will be given a positive integer, val and you have to create the function next_pal()(nextPal Javascript) that will output the smallest palindrome number higher than val.
+
+// Let's see:
+
+// For Ruby
+// next_pal(11) == 22
+
+// next_pal(188) == 191
+
+// next_pal(191) == 202
+
+// next_pal(2541) == 2552
+// You will be receiving values higher than 10, all valid.
+
+// Enjoy it!!
+
+function nextPal(val) {
+    for(let i = val + 1; i > val; i++){
+      if(String(i).split('').reverse().join('') === String(i).split('').join('')){
+         return i
+        }
+    }
+  }
