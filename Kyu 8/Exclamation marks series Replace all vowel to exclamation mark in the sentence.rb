698417f152290ee0b78d54df8fb5b94d1870134e
Vickhammer <tvickhammer@gmail.com>
2023-04-20 12:00:00 -0700

    Kyu 8

---
+++ Kyu 8/Exclamation marks series Replace all vowel to exclamation mark in the sentence.rb
@@ -0,0 +1,19 @@
+# DESCRIPTION:
+# Description:
+# Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+
+# Examples
+# replace("Hi!") === "H!!"
+# replace("!Hi! Hi!") === "!H!! H!!"
+# replace("aeiou") === "!!!!!"
+# replace("ABCDE") === "!BCD!"
+
+def replace(s)
+    sArray = s.split('')
+    sArray.each_with_index do |l,idx|
+      if 'aeiouAEIOU'.include?(l)
+        sArray[idx] = '!'
+      end
+    end
+    return sArray.join('')
+  end
