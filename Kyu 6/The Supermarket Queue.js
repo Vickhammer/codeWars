2841de0423b1f52824466e39f6c4f93c4e823c92
Vickhammer <tvickhammer@gmail.com>
2023-09-30 12:00:00 -0700

    Kyu 6

---
+++ Kyu 6/The Supermarket Queue.js
@@ -0,0 +1,26 @@
+// DESCRIPTION:
+// There is a queue for the self-checkout tills at the supermarket. Your task is write a function to calculate the total time required for all the customers to check out!
+
+// input
+// customers: an array of positive integers representing the queue. Each integer represents a customer, and its value is the amount of time they require to check out.
+// n: a positive integer, the number of checkout tills.
+// output
+// The function should return an integer, the total time required.
+
+function queueTime(customers, n) {
+    if (customers.length == 0){
+      return 0
+    }
+    else if(n < customers.length){
+      let time = new Array(n).fill(0);
+      for(let i = 0; i < customers.length; i++){
+        let index = time.indexOf(Math.min(...time))
+        time[index] += customers[i]
+      }
+      return Math.max(...time)
+  
+    }
+    else{
+      return (Math.max(...customers))
+  }
+  }
